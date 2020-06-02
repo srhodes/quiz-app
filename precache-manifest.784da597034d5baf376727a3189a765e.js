e03bcd15f19532703ce8c307e7a3ec17962e5216
srhodes <rhodestephane@comcast.net>
2020-06-02 12:14:52 -0700

    Updates

---
+++ precache-manifest.784da597034d5baf376727a3189a765e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1155293970054827a3e976c1a7e71fe",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "7fcbf2b81b330d952b8c",
+    "url": "/quiz-app/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "676b208e46f84c98e4d4",
+    "url": "/quiz-app/static/js/2.ad7aaad7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.ad7aaad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fcbf2b81b330d952b8c",
+    "url": "/quiz-app/static/js/main.8d2f3229.chunk.js"
+  },
+  {
+    "revision": "6b7c0ff1e9d031402481",
+    "url": "/quiz-app/static/js/runtime-main.f9d8d9b5.js"
+  }
+]);
